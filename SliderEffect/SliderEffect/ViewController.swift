5850db09b09e17db54a47435f8c6708ba40b87e8
BUCSZ <shenqinghao92@Gmail.com>
2017-07-01 09:17:17 -0700

    priv clear

---
+++ SliderEffect/SliderEffect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SliderEffect
+//
+//  Created by FoxZzz on 2017-06-28.
+//  Copyright © 2017 FoxZzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var CenterSld: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
